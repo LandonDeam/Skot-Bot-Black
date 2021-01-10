70c75d35f2ef0a3a522200cf198c4f5ad8eb1c39
Landon Deam <landon.deam@gmail.com>
2021-01-10 04:00:20 -0500

    .gitignore added to my token (bruh)
    also changed somethin with tha mald command

--- src/Config.ts
+++ src/Config.ts
@@ -1,3 +1,3 @@
 export let token: string = "NTgxMjAzNDY3NDczOTExODMx.XOb16A.q5zpQ_WvNeZKhKQEC5oOJi8magA";
 export let prefix: string = ".";
-export let owners: string[] = [];
+export let owners: string[] = ["245659499292131349"];
--- src/commands/Public Commands/MaldCommand.ts
+++ src/commands/Public Commands/MaldCommand.ts
@@ -8,7 +8,7 @@ export default class MaldCommand extends Command {
             aliases: ["mald"],
             category: "Public Commands",
             description: {
-                content: "Check the latency of the ping to the Discord API",
+                content: "Lets Skotty know he's malding",
                 usage: "mald",
                 examples: [
                     "mald"
@@ -21,6 +21,6 @@ export default class MaldCommand extends Command {
     public exec(message: Message): Promise<Message> {
         //("707447475228639267").send("<@188362544283516928>");
         message.util.send("Skotty's malding bro");
-        return (this.client.channels.cache.get('707447475228639267') as TextChannel).send('<@188362544283516928>')
+        return (this.client.channels.cache.get('707447475228639267') as TextChannel).send('<@188362544283516928>');
     }
 }
