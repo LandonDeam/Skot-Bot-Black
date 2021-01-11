0e1e4bdf2786a56ed75dcd8a6705c4dd9224d6c3
Landon Deam <landon.deam@gmail.com>
2021-01-11 18:59:26 -0500

    b

--- src/models/Warns.ts
+++ src/models/Warns.ts
@@ -1,7 +1,7 @@
 import {Entity, Column, PrimaryGeneratedColumn} from "typeorm";
 
 @Entity("warns")
-export class Warns { 
+export class Warns {
     @PrimaryGeneratedColumn()
     id!: number;
     
