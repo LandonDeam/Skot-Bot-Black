637944a4c6ae6ddebead6a7859f4d7987512ac73
Landon Deam <landon.deam@gmail.com>
2021-01-29 23:25:01 -0500

    Added Give Command

---
+++ src/commands/Public Commands/GiveCommand.ts
@@ -0,0 +1,55 @@
+import {Command} from "discord-akairo";
+import {Message, MessageEmbed, GuildMember} from "discord.js";
+import { Repository } from "typeorm";
+import {Balance} from "../../models/Balance";
+import BalanceManager from "../../structures/economy/BalanceManager";
+
+export default class GiveCommand extends Command {
+    public constructor() {
+        super("give", {
+            aliases: ["give","g"],
+            category: "Public Commands",
+            description: {
+                content: "Give specified amount of currency to another member",
+                usage: "give [member] [money]",
+                examples: [
+                    "give @Host#0001 200",
+                    "give host 1337"
+                ]
+            },
+            ratelimit: 5,
+            args: [
+                {
+                    id: "member",
+                    type: "member"
+                },
+                {
+                    id: "money",
+                    type: "number"
+                }
+            ]
+        });
+    }
+
+    public async exec(message: Message, {member, money}: {member: GuildMember, money: number}): Promise<Message> {
+        const balanceRepo: Repository<Balance> = this.client.db.getRepository(Balance);
+        let receiver: Balance = await BalanceManager.getUser(balanceRepo, member);
+        let sender: Balance = await BalanceManager.getUser(balanceRepo, message.member);
+
+        if(money > Number(sender.bal)) {
+            await BalanceManager.add(balanceRepo, message.member, -money);
+            await BalanceManager.add(balanceRepo, member, money);
+            return message.util.send(new MessageEmbed()
+                .setAuthor(`Western Union`)
+                .setDescription(`${message.member.nickname} successfully gave ${member.nickname} ${(money).toLocaleString('en-us')}GH₵!`)
+                .setColor("4caf50")
+            );
+        }
+        return message.util.reply(new MessageEmbed()
+            .setAuthor(`Western Union`)
+            .setDescription(`**Failed to send ${(money).toLocaleString('en-us')}GH₵ to ${member.nickname}, insufficient funds in user account.**\n`+
+                            `You have: ${(sender.bal).toLocaleString('en-us')}GH₵`)
+            .setColor("f44336")
+        );
+    }
+}
--- src/commands/Public Commands/WithdrawCommand.ts
+++ src/commands/Public Commands/WithdrawCommand.ts
@@ -30,6 +30,7 @@ export default class WithdrawCommand extends Command {
             return message.util.reply(`You have no money in the bank to withdraw!`);
         }
         else if(Number(user.timeDeposited) + Number(user.time) > Date.now()) {
+
             const ends: number = Number(user.timeDeposited);
             return  message.util.send(new MessageEmbed()
             .setAuthor(`Bank | ${message.member.user.tag}`)
@@ -39,6 +40,7 @@ export default class WithdrawCommand extends Command {
             .setFooter(`Deposited`)
             .setTimestamp(ends)
             );
+
         } //Math.pow(2.5, (Math.random() / 12) * Math.log10((Number(user.time) / 86400000) + 1))
         let multiplier: number = 0.2 * Math.log(12 * (Math.random() * .99 + .01) * (Number(user.time) / 8640000) + 100) + 0.079;
         let money: number = Number((await BalanceManager.getUser(balanceRepo, message.member)).bal);
@@ -46,7 +48,7 @@ export default class WithdrawCommand extends Command {
         return message.util.send(new MessageEmbed()
         .setAuthor(`Withdrawal | ${message.member.user.tag}`)
         .setColor("#4caf50")
-        .setDescription(`Withdrawn: *${(withdrawn).toLocaleString('en-us')}GH₵*\n`+
+        .setDescription(`Withdrew: *${(withdrawn).toLocaleString('en-us')}GH₵*\n`+
                         `New Balance: ${(money + withdrawn).toLocaleString('en-us')}GH₵`)
         );
     }
