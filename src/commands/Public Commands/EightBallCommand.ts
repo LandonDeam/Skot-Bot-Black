07ec2a46230e0a42ce11f4b4799f53fc67dbde58
Landon Deam <landon.deam@gmail.com>
2021-03-07 22:35:03 -0500

    Added 8ball command

---
+++ src/commands/Public Commands/EightBallCommand.ts
@@ -0,0 +1,46 @@
+import {Command} from "discord-akairo";
+import {Message} from "discord.js";
+import { Double } from "typeorm";
+
+export default class EightBallCommand extends Command {
+    public constructor() {
+        super("eightball", {
+            aliases: ["eightball", "8ball", "8b"],
+            category: "Public Commands",
+            description: {
+                content: "Chooses a random eight-ball like response to a prompt",
+                usage: "8ball <message>",
+                examples: [
+                    "8ball will alex give me money for free"
+                ]
+            },
+            ratelimit: 10
+        });
+    }
+
+    public exec(msg: Message) {
+        let outcome: String[] = [
+            "As I see it, yes.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don’t count on it.",
+            "It is certain.",
+            "It is decidedly so.",
+            "Most likely.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Outlook good.",
+            "Reply hazy, try again.",
+            "Signs point to yes.",
+            "Very doubtful.",
+            "Without a doubt.",
+            "Yes.",
+            "Yes – definitely.",
+            "You may rely on it."
+        ]
+        msg.util.send(outcome[Math.floor(Math.random() * outcome.length)]);
+    }
+}
